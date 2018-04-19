6281f5d82079e635b5a9977295c9fb0e1e8a6f09
odssey <odssey@ya.ru>
2018-04-19 23:12:32 +0200

    final version

---
+++ README.md
@@ -0,0 +1,44 @@
+## README DE BASE DU SEUL GROUPE THP SESSION4 #LYON
+# DAY 12
+Hello et bienvenue sur notre github, excellent correction !
+
+Theme de jour : Faire un morpion (simple sur le papier, cass c@#{~# en pratique...)
+
+
+* Atom SURTOUT PAS sublime -payant et moins de packages ultra praticools comme auto indent ou linter ;)
+* et la super team #Lyon06
+
+## Githoub des peer-learning :
+
+* Mohammad-Ali
+* Odssey
+* oseus
+* Hug-O
+
+## Slack THP des contributeurs :
+
+* Mohammad-Ali : @Mohammad-Ali
+* Odyssée Levine : @Odyssée Levine
+* David Coat : @Coat David
+* Hugo : @Hugo
+
+
+## Infos importantes sur le programme
+Après s'être saigné on a pas réussi 
+à entrer toutes les victoires dans un seul array
+  victory_array.true = [
+    [6,7,8],
+    [6,4,2],
+    [0,1,2],
+    [3,4,5],
+    [0,4,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8]
+  ]
+  if victory_array == true
+    puts "Game Over"
+    puts "Encore une partie (O)ui (N)on?"
+donc on en a fait 8 tout dégueu avec les booleans :/
+Si vous avez trouvé le moyen de mettre les victories en 1 array comme demandé par Féfé
+Merci de nous contacter! Please , Thanks/
---
+++ morbackTHP.rb
@@ -0,0 +1,167 @@
+class BoardCase
+	# La classe a 2 attr_accessor, sa val, ainsi que son numéro de case
+	attr_accessor :value, :numero_de_case
+
+	def initialize(numero_de_case)
+		@value = " "
+		@numero_de_case = numero_de_case
+	end
+end
+
+class Board
+
+	attr_accessor 	:plateau
+  # Quand la classe s'initialize, elle doit créer 9 instances BoardCases ici @case1--case9
+  # Ces instances sont rangées dans une array qui est l'attr_accessor de la classe ici @plateau
+	def initialize
+		@case1 = BoardCase.new(1)
+		@case2 = BoardCase.new(2)
+		@case3 = BoardCase.new(3)
+		@case4 = BoardCase.new(4)
+		@case5 = BoardCase.new(5)
+		@case6 = BoardCase.new(6)
+		@case7 = BoardCase.new(7)
+		@case8 = BoardCase.new(8)
+		@case9 = BoardCase.new(9)
+		@plateau = [@case1.value, @case2.value, @case3.value, @case4.value, @case5.value, @case6.value, @case7.value, @case8.value, @case9.value]
+	end
+
+	def play(joueur)
+		# On choisi ici d'uiliser la méthode include pour vérifier l'état de la case si elle renvoit bien une dispo " "
+		if @plateau.include? " "
+			# Notre array tableau n'accepte que des chiffres donc on convertie systématiquement l'input du joueur en integer avec to_i
+			input = gets.chomp.to_i
+		#Si le choix de case par l'un des deux joueurs renvoie à la  victory == true alors la partie se termine
+			if (@plateau[input - 1] === " ")
+				@plateau[input - 1] = joueur.value
+				victory(joueur)
+
+		#Si la case est déjà prise la console CLI renvoie un message d'erreur
+			else
+				puts "Essaie encore!"
+				play(joueur)
+			end
+	  #Si la case est déjà prise ET que toutes les cases sont prises ET qu'il n'y  a pas de vainqueur :
+		else
+			puts "Match nul, honneur aux chanceux et aux challengers"
+			exit
+		end
+
+	end
+
+	def display
+		# On divise le plateau en 3 array pour chacune on coince un espace et un string par case avec .each do
+		@plateau[0..2].each do |val|
+			print "|" + val + " "
+		end
+		print "|"
+		print "\n"
+		print "----------"
+		print "\n"
+		@plateau[3..5].each do |val|
+			print "|" + val + " "
+		end
+		print "|"
+		print "\n"
+		print "----------"
+		print "\n"
+		@plateau[6..8].each do |val|
+			print "|" + val + " "
+		end
+
+		print "|"
+		print "\n"
+		puts " "
+
+	end
+	# On range ici les combinaisons gagnantes par array
+	def victory(joueur)
+
+		if (plateau[0] == joueur.value) && (plateau[1] == joueur.value) && (plateau[2] == joueur.value)
+			puts "#{joueur.name} a gagné la partie."
+			display		# <= Pour réaficher le tableau.
+			exit		# <= Pour quitté le jeu si la condition est vrai et qu'un joueur a gagné
+
+		elsif (plateau[3] == joueur.value) && (plateau[4] == joueur.value) && (plateau[5] == joueur.value)
+			puts "#{joueur.name} a gagné la partie."
+			display
+			exit
+		elsif (plateau[0] == joueur.value) && (plateau[3] == joueur.value) && (plateau[6] == joueur.value)
+			puts "#{joueur.name} a gagné la partie."
+			display
+			exit
+		elsif (plateau[2] == joueur.value) && (plateau[4] == joueur.value) && (plateau[6] == joueur.value)
+			puts "#{joueur.name} a gagné la partie."
+			display
+			exit
+		elsif (plateau[0] == joueur.value) && (plateau[4] == joueur.value) && (plateau[8] == joueur.value)
+			puts "#{joueur.name} a gagné la partie."
+			display
+			exit
+		elsif (plateau[2] == joueur.value) && (plateau[5] == joueur.value) && (plateau[8] == joueur.value)
+			puts "#{joueur.name} a gagné la partie."
+			display
+			exit
+		elsif (plateau[1] == joueur.value) && (plateau[4] == joueur.value) && (plateau[7] == joueur.value)
+			puts "#{joueur.name} a gagné la partie."
+			display
+			exit
+		else
+			return
+		end
+	end
+
+end
+
+class Player
+	attr_accessor :name, :value
+	attr_writer :he_won
+
+	def initialize(name, value)		# <= Initilaise les joueurs.
+		@name = name
+		@value = value
+	end
+
+end
+
+class Game
+	attr_accessor :joueur
+
+	def initialize		# <= Initialise la partie.
+		@joueur = joueur
+		puts "T__tic__H__tac__P__toe__Team_Lyon06!"
+		puts "Jouez les positions comme suit:"
+		puts "1 | 2 | 3",
+				 "---------",
+		     "4 | 5 | 6",
+		     "---------",
+		     "7 | 8 | 9"
+		print "\n"
+		# end
+		puts "Hello le chanceux(tu commences en 1er :Ъ), quel est ton pseudo ?"
+		joueur1 = gets.chomp
+		puts "Hello l'adversaire(courage le challenger!), quel est ton pseudo ?"
+		joueur2 = gets.chomp
+		@joueur1 = Player.new(joueur1, "X")
+		@joueur2 = Player.new(joueur2, "O")
+		@nouveau_plateau = Board.new
+	end
+
+	def go
+		turn
+	end
+
+	def turn
+		while (true)		# <= Continu le jeu tant que les conditions d'arret plus haut dans le code ne sont pas respecter.
+			@nouveau_plateau.display
+			puts "#{@joueur1.name} honneur au chanceux, ton choix ?"
+			@nouveau_plateau.play(@joueur1)
+			@nouveau_plateau.display
+			puts "#{@joueur2.name} honneur au challenger, ton choix?"
+			@nouveau_plateau.play(@joueur2)
+		end
+	end
+
+end
+
+Game.new.go		#
